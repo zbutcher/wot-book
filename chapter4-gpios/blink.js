b70fb4cc9565bd40be33c2db1fbf4b10e4bf2339
zbutcher <zstuff@mac.com>
2016-08-19 17:45:06 -0700

    Sync Test

--- chapter4-gpios/blink.js
+++ chapter4-gpios/blink.js
@@ -19,6 +19,7 @@ process.on('SIGINT', function () { //#F
   process.exit();
 });
 
+// TEST
 // #A Import the onoff library
 // #B Initialize pin 4 to be an output pin
 // #C This interval will be called every 2 seconds
